637a02474adc15566c3e9c44fbbf75fdfb968812
Bekzod <bekzodisakov18@gmail.com>
2023-04-25 14:47:54 +0500

    edit initial state

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,69 @@ import * as React from "react";
 import { Column } from "./components/Column";
 import Header from "./components/Header";
 
-const INITIAL_LIST = [
+export const PRIORITIES = ["low", "middle", "high"];
+export const PRIORITY_COLORS = {
+  low: "#22c55e",
+  middle: "#3b82f6",
+  high: "#ef4444",
+};
+
+export const ASSIGNEES = new Map([
+  [
+    "assigneeId1",
+    {
+      id: "assigneeId1",
+      firstName: "Robin",
+      lastName: "Wieruch",
+      imgUrl: ''
+    },
+  ],
+  [
+    "assigneeId2",
+    {
+      id: "assigneeId2",
+      firstName: "Jannet",
+      lastName: "Layn",
+      imgUrl: ''
+    },
+  ],
+  [
+    "assigneeId3",
+    {
+      id: "assigneeId3",
+      firstName: "Aiden",
+      lastName: "Kettel",
+      imgUrl: ''
+    },
+  ],
+]);
+
+const CARDS = [
   {
     id: "1",
-    firstName: "Robin",
-    lastName: "Wieruch",
+    task: "Deploy the app",
+    description: "This is the dastk description for task id 1",
+    assigneesIds: ["assigneeId1"],
+    priorityIdx: 0,
   },
   {
     id: "2",
-    firstName: "Aiden",
-    lastName: "Kettel",
+    task: "Change the styles",
+    description: "This is the dastk description for task id 2",
+    assigneesIds: ["assigneeId2", "assigneeId3"],
+    priorityIdx: 2,
   },
   {
     id: "3",
-    firstName: "Jannet",
-    lastName: "Layn",
+    task: "Learn databases",
+    description: "This is the dastk description for task id 3",
+    assigneesIds: ["assigneeId3"],
+    priorityIdx: 1,
   },
 ];
 
 const App = () => {
-  const [list, setList] = React.useState(INITIAL_LIST);
+  const [list, setList] = React.useState(CARDS);
 
   const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
--- src/components/DraggableItem.jsx
+++ src/components/DraggableItem.jsx
@@ -1,8 +1,13 @@
+import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import clsx from "clsx";
+
+import { ASSIGNEES, PRIORITIES, PRIORITY_COLORS } from "../App";
 import GripIcon from "./icons/GripIcon";
 
 const DraggableItem = ({ item, index, dragItemClasses, children }) => {
+  const assignees = item?.assigneesIds.map((id) => ASSIGNEES.get(id));
+
   return (
     <Draggable key={item.id} index={index} draggableId={item.id}>
       {(provided, snapshot) => (
@@ -17,8 +22,30 @@ const DraggableItem = ({ item, index, dragItemClasses, children }) => {
             ...provided.draggableProps.style,
           }}
         >
-          {item.firstName} {item.lastName}
-          <div className='inline-block px-0.5 py-1 border-2' {...provided.dragHandleProps}>
+          <div
+            className={`w-6 h-2 rounded-md`}
+            style={{
+              backgroundColor: PRIORITY_COLORS[PRIORITIES[item.priorityIdx]],
+            }}
+          ></div>
+          <h3>{item.task}</h3>
+          <p>{item.description}</p>
+          <div>
+            {assignees &&
+              assignees.map((a) => (
+                <div key={a.id}>
+                  <img
+                    src={`https://eu.ui-avatars.com/api/?name=${a.firstName}+${a.lastName}&size=80`}
+                    alt={a.firstName + " " + a.lastName}
+                    title={a.firstName + " " + a.lastName}
+                  />
+                </div>
+              ))}
+          </div>
+          <div
+            className='inline-block px-0.5 py-1 border-2'
+            {...provided.dragHandleProps}
+          >
             <GripIcon className={"w-4 h-4"} />
           </div>
         </div>
